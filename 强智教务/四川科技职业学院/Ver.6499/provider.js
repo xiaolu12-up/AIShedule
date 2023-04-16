d06cd5570cf01a56fc0bcc1d19ea3ed7d3d8b156
xiaolu <115175005+xiaolu52@users.noreply.github.com>
2023-04-16 14:12:58 +0800

    第一次适配

---
+++ 强智教务/四川科技职业学院/Ver.6499/provider.js
@@ -0,0 +1,10 @@
+function scheduleHtmlProvider(
+  iframeContent = '',
+  frameContent = '',
+  dom = document
+) {
+  //除函数名外都可编辑
+  //以下为示例，您可以完全重写或在此基础上更改
+  const table = dom.getElementsByTagName("table")[1]
+  return table.outerHTML
+}
