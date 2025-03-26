74917ed4177e0927d992b89c0a3774759927c253
lu52 <597576415@qq.com>
2025-03-27 00:18:23 +0800

    首次适配

---
+++ 正方教务/闽南师范大学/Ver.14886/provider.js
@@ -0,0 +1,82 @@
+async function request(tag, data, url) {
+  return await fetch(url, {
+    method: tag,
+    body: data,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+    .then((rp) => rp.text())
+    .then((v) => v)
+}
+function AIScheduleLoading({
+  titleText = '加载中',
+  contentText = 'loading...',
+} = {}) {
+  console.log('start......')
+  AIScheduleComponents.addMeta()
+  const title = AIScheduleComponents.createTitle(titleText)
+  const content = AIScheduleComponents.createContent(contentText)
+  const card = AIScheduleComponents.createCard([title, content])
+  const mask = AIScheduleComponents.createMask(card)
+
+  let dyn
+  let count = 0
+  function dynLoading() {
+    let t = ['loading', 'loading.', 'loading..', 'loading...']
+    if (count == 4) count = 0
+    content.innerText = t[count++]
+  }
+
+  this.show = () => {
+    console.log('show......')
+    document.body.appendChild(mask)
+    dyn = setInterval(dynLoading, 1000)
+  }
+  this.close = () => {
+    document.body.removeChild(mask)
+    clearInterval(dyn)
+  }
+}
+async function scheduleHtmlProvider(
+  iframeContent = '',
+  frameContent = '',
+  dom = document
+) {
+  //除函数名外都可编辑
+  //以下为示例，您可以完全重写或在此基础上更改
+  let ts = `进入教务系统点击导入
+  如导入失败，请联系Q：597576415`
+  //     alert(ts)
+
+  await loadTool('AIScheduleTools')
+  let loadd = new AIScheduleLoading()
+  loadd.show()
+  let htt = null
+  let xnm = ''
+  let xqm = ''
+  try {
+    let forms = dom.getElementById('area_one')
+    xnm = forms.querySelector("#xnm").value
+    xqm = forms.querySelector("#xqm").value
+  } catch {
+    let forms = dom.getElementById('ajaxForm')
+    xnm = forms.xnm.value
+    xqm = forms.xqm.value
+    if (!xnm) {
+      await AIScheduleAlert(ts)
+      loadd.close()
+      return 'do not continue'
+    }
+  }
+  htt = JSON.parse(
+    await request(
+      'post',
+      'xnm=' + xnm + '&xqm=' + xqm,
+      '/jwglxt/kbcx/xskbcx_cxXsgrkb.html'
+    )
+  )
+  console.log(htt);
+  loadd.close()
+  return JSON.stringify({ listArr: htt.kbList, xqm: xqm, xnm: xnm })
+}
